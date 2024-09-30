c6fa866c0269338b01c8bc292bf89ce8f63730e8
ganeshdoke001 <ganeshdoke1999@gmail.com>
2024-09-30 09:50:47 +0530

    v1:3448, created payment service project

---
+++ src/main/java/com/inn/pay/Eai3448PayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.inn.pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eai3448PayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eai3448PayServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inn/pay/Eai3448PayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inn.pay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eai3448PayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
